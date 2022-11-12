934f5e44c42e96b2271627c9d61a22a04005205b
Tomo-Shortty <tomo_007@live.com.au>
2022-11-12 15:56:38 +1000

    Restarting project build

--- FootballSeasonBuilder/FootballSeasonBuilder/Models/Round.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FootballSeasonBuilder.Models
-{
-    public class Round
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public League League { get; set; }
-    }
-}
--- FootballSeasonBuilder/FootballSeasonBuilder/Models/SeasonList.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FootballSeasonBuilder.Models
-{
-    public class SeasonList
-    {
-        public List<League> Leagues { get; set; }
-        public List<Team> Teams { get; set; }
-        public List<Player> Players { get; set; }
-    }
-}
